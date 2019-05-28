20289dc08962cada2a0d4138e6d74cc4d6ba4be7
aanimesh23 <animesha@uci.edu>
2019-05-28 12:08:25 -0700

    pickle is useless so JSON

--- Michael_inverter.py
+++ Michael_inverter.py
@@ -115,7 +115,6 @@ if __name__ == '__main__':
     i.start_indexing()
     i.calculate_document_frequency()
     i.calculate_tfidf()
-    i.get_tfidfDict()
     inverted = i.get_tfidfDict()
-    with open("invertedIndex.pickle", 'wb') as handle:
-        pickle.dump(inverted, handle, protocol=pickle.HIGHEST_PROTOCOL)
+    with open('invertedIndex.json', 'wb') as f:
+        json.dump(inverted, f)
