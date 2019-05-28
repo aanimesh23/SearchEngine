2961b68760f592296e9b594dd15f7d28cf8718d1
Michael Kirk <michael_c_kirk@yahoo.com>
2019-05-28 13:48:39 -0700

    working retreiver

--- retreiver.py
+++ retreiver.py
@@ -1,6 +1,8 @@
 import json
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer, PorterStemmer
+from collections import defaultdict
+import re
 
 class Retreiver:
     """
@@ -10,37 +12,42 @@ class Retreiver:
     	self.invertedIndex = {}
     	self.sorted_urls = {}
     	self.lemmatizer = WordNetLemmatizer()
-        self.stopWords = set(stopwords.words('english'))
-        self.porterStemmer = PorterStemmer()
+    	self.stopWords = set(stopwords.words('english'))
+    	self.porterStemmer = PorterStemmer()
 
     def open_inverted_index(self, filename):
-    	json_file = open()
-    	json_str = json_file.read(filename)
-    	json.dict = json.loads(json_str)[0]
+    	json_file = open(filename)
+    	json_str = json_file.read()
+    	json.dict = json.loads(json_str)
     	self.invertedIndex = json.dict
 
     def retreive_urls(self, query):
     	url_scores = defaultdict(float)
-        tokenized = re.split('[^a-zA-z0-9]+', url_text)
-        for token in tokenized:
-            if token not in self.stopWords:
-                token = token.lower()
-                token = self.lemmatizer.lemmatize(token)
-                token = self.porterStemmer.stem(token)
-            	if token in self.tfidfDict.keys():
-                	for url, tfidf in self.tfidfDict[token].items():
-                    	url_scores[url] += tfidf
+    	tokenized = re.split('[^a-zA-z0-9]+', query)
+    	for token in tokenized:
+    		if token not in self.stopWords:
+    			token = token.lower()
+    			token = self.lemmatizer.lemmatize(token)
+    			token = self.porterStemmer.stem(token)
+    			if token in self.invertedIndex.keys():
+    				for url, tfidf in self.invertedIndex[token].items():
+    					url_scores[url] += tfidf
 
-        url_scores = sorted(url_scores.items(), key = lambda x: x[1], reverse = True)
-        self.sorted_urls = url_scores
+    	url_scores = sorted(url_scores.items(), key = lambda x: x[1], reverse = True)
+    	self.sorted_urls = url_scores
 
     def get_top_urls(self):
-    	urlLength = len(sorted_urls)
+    	urlLength = len(self.sorted_urls)
     	if urlLength < 20:
-    		print(sorted_urls)
+    		print(self.sorted_urls)
     	else:
     		i = 1
     		while i <= 20:
-	    		for url,value in self.sorted_urls.items():
+	    		for url,value in self.sorted_urls:
 	    			print(i, url)
 	    			i+=1
+
+r = Retreiver()
+r.open_inverted_index("invertedIndex.json")
+r.retreive_urls("donald bren")
+r.get_top_urls()
