e73d2d50a9dd2fe17fde68cfa05034e16b0426ce
rslam8 <34228328+rslam8@users.noreply.github.com>
2019-05-30 12:50:10 -0700

    fixed indentation in retriever and added search result html

--- retreiver.py
+++ retreiver.py
@@ -18,31 +18,31 @@ class Retreiver:
         self.porterStemmer = PorterStemmer()
 
     def open_inverted_index(self, filename):
-    	json_file = open(filename)
-    	json_str = json_file.read()
-    	json.dict = json.loads(json_str)
-    	self.invertedIndex = json.dict
+        json_file = open(filename)
+        json_str = json_file.read()
+        json.dict = json.loads(json_str)
+        self.invertedIndex = json.dict
 
     def retreive_urls(self, query):
-    	url_scores = defaultdict(float)
-    	tokenized = re.split('[^a-zA-z0-9]+', query)
-    	for token in tokenized:
-    		if token not in self.stopWords:
-    			token = token.lower()
-    			token = self.lemmatizer.lemmatize(token)
-    			token = self.porterStemmer.stem(token)
-    			if token in self.invertedIndex.keys():
-    				for url, tfidf in self.invertedIndex[token].items():
-    					url_scores[url] += tfidf
+        url_scores = defaultdict(float)
+        tokenized = re.split('[^a-zA-z0-9]+', query)
+        for token in tokenized:
+            if token not in self.stopWords:
+                token = token.lower()
+                token = self.lemmatizer.lemmatize(token)
+                token = self.porterStemmer.stem(token)
+                if token in self.invertedIndex.keys():
+                    for url, tfidf in self.invertedIndex[token].items():
+                        url_scores[url] += tfidf
 
-    	url_scores = sorted(url_scores.items(), key = lambda x: x[1], reverse = True)
-    	return url_scores
+        url_scores = sorted(url_scores.items(), key = lambda x: x[1], reverse = True)
+        return url_scores
 
     def get_top_urls(self, sorted_urls, max = 20):
-    	urlLength = len(sorted_urls)
-    	if urlLength < max:
-    		print(sorted_urls)
-    	else:
+        urlLength = len(sorted_urls)
+        if urlLength < max:
+            print(sorted_urls)
+        else:
             i = 1
             for url,value in sorted_urls:
                 if (i <= max):
