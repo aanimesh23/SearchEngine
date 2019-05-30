dee479b8cf5cd1ced5e47f362bb1560b608d888a
aanimesh23 <animesha@uci.edu>
2019-05-30 14:20:35 -0700

    added some flask functionality

--- flask_app.py
+++ flask_app.py
@@ -6,5 +6,12 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+
+@app.route('/results', methods = ['POST'])
+def results():
+	query = request.form['searchTerm']
+	print(query)
+	return render_template('results.html')
+
 if __name__ == '__main__':
     app.run()
